1c4ea2348f71d38be02070453ba32c9524968c38
Warren R Bank <warren.r.bank@gmail.com>
2020-01-24 15:54:13 -0800

    v0.1.0

---
+++ New-York-Times/content.js
@@ -0,0 +1,67 @@
+var payload = function(){
+  // stop after 1 minute if no paywall is found when DOM is polled every 0.5 seconds
+  let timeout = 120
+
+  const remove_pay_wall = () => {
+    document.querySelector('#gateway-content').remove()
+
+    // .css-1bd8bfl
+    document.querySelector('#app > div > div > div:nth-child(3)').remove()
+
+    // .css-mcm29f
+    document.querySelector('#app > div > div').className = ''
+  }
+
+  const test_paywall = () => {
+    if (document.querySelector('#gateway-content'))
+      remove_pay_wall()
+    else
+      run_timer()
+  }
+
+  const run_timer = () => {
+    if (timeout > 0) {
+      timeout--
+      setTimeout(test_paywall, 500)
+    }
+  }
+
+  run_timer()
+}
+
+var get_hash_code = function(str){
+  var hash, i, char
+  hash = 0
+  if (str.length == 0) {
+    return hash
+  }
+  for (i = 0; i < str.length; i++) {
+    char = str.charCodeAt(i)
+    hash = ((hash<<5)-hash)+char
+    hash = hash & hash  // Convert to 32bit integer
+  }
+  return Math.abs(hash)
+}
+
+var inject_function = function(_function){
+  var inline, script, head
+
+  inline = _function.toString()
+  inline = '(' + inline + ')()' + '; //# sourceURL=crx_extension.' + get_hash_code(inline)
+  inline = document.createTextNode(inline)
+
+  script = document.createElement('script')
+  script.appendChild(inline)
+
+  head = document.head
+  head.appendChild(script)
+}
+
+if (document.readyState === 'complete'){
+  inject_function(payload)
+}
+else {
+  document.addEventListener("DOMContentLoaded", function(event) {
+    inject_function(payload)
+  })
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+### [Chromium browser extension for The New York Times](https://github.com/warren-bank/crx-New-York-Times)
+
+Read articles on [_The New York Times_ website](https://www.nytimes.com/) without a paywall.
+
+#### Legal:
+
+* copyright: [Warren Bank](https://github.com/warren-bank)
+* license: [GPL-2.0](https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt)
